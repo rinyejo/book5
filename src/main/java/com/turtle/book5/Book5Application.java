4ecbbe54b9263032458888060cda1683fe4e069f
rinyejo <rinyejoin@gmail.com>
2023-12-11 14:52:29 +0900

    init

---
+++ src/main/java/com/turtle/book5/Book5Application.java
@@ -0,0 +1,13 @@
+package com.turtle.book5;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Book5Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Book5Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/turtle/book5/Book5ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.turtle.book5;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Book5ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
